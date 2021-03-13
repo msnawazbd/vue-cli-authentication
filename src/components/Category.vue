c394397590ccf3b6f1bf75f6f1714613fecde2e0
MS Nawaz <msnawazbd@gmail.com>
2021-03-13 20:17:19 +0600

    Added Component

---
+++ src/components/Category.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-md-4 col-sm-12">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title mb-0">Add Category</h5>
+        </div>
+        <div class="card-body">
+          <!-- login form -->
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <button type="submit" class="btn btn-warning btn-block">
+              Submit
+            </button>
+          </form>
+          <!-- ./end login form -->
+        </div>
+        <div class="card-footer"></div>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-12">
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title mb-0">Manage Categories</h5>
+        </div>
+        <div class="card-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="card-footer"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+};
+</script>
+
+<style scoped>
+</style>
